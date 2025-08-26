1927f32aead4ad65f161eafa3f999db0aca0d7c4
Emeka Manuel <marrnuel123@gmail.comm>
2025-08-26 14:56:31 +0100

    Refactor UserNav component by removing logout functionality and unused imports
    - Eliminated the logout function and associated UI elements from the UserNav component.
    - Removed the LogOut icon import as it is no longer needed.

--- src/components/user-nav.tsx
+++ src/components/user-nav.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { ChevronDown, LogOut, User } from "lucide-react";
+import { ChevronDown, User } from "lucide-react";
 import Link from "next/link";
 
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
@@ -20,12 +20,9 @@ import {
 } from "@/components/ui/tooltip";
 import UserImage from "../../public/images/profile.png";
 
-import { useAuth } from "@/providers/auth-provider";
 import Image from "next/image";
 
 export function UserNav() {
-  const { logout } = useAuth();
-
   return (
     <DropdownMenu>
       <TooltipProvider disableHoverableContent>
@@ -61,10 +58,6 @@ export function UserNav() {
           </DropdownMenuItem>
         </DropdownMenuGroup>
         <DropdownMenuSeparator />
-        <DropdownMenuItem className="hover:cursor-pointer" onClick={logout}>
-          <LogOut className="w-4 h-4 mr-3 text-muted-foreground" />
-          Sign out
-        </DropdownMenuItem>
       </DropdownMenuContent>
     </DropdownMenu>
   );
